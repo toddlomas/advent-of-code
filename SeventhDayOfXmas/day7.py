5f3634ed19498de10b9db402d7a01fc84164c0b7
Todd Lomas <todd.lomas@hotmail.co.uk>
2022-12-07 17:02:47 +0000

    Day7 - couldnt do it, followed a tutorial

---
+++ SeventhDayOfXmas/day7.py
@@ -0,0 +1,78 @@
+'''
+Problem statement:
+Find all of the directories with a total size of at most 100000.
+What is the sum of the total sizes of those directories?
+'''
+from collections import defaultdict
+from functools import lru_cache
+from pprint import pprint
+
+with open("/Users/toddlomas/Documents/AdventOfCode/seventhDayOfXmas/day7-input.txt") as fin:
+    blocks = ("\n" + fin.read().strip()).split("\n$ ")[1:]
+
+path = []
+
+dir_sizes = defaultdict(int)
+children = defaultdict(list)
+
+
+def parse(block):
+    lines = block.split("\n")
+    command = lines[0]
+    outputs = lines[1:]
+
+    parts = command.split(" ")
+    op = parts[0]
+    if op == "cd":
+        if parts[1] == "..":
+            path.pop()
+        else:
+            path.append(parts[1])
+
+        return
+
+    abspath = "/".join(path)
+    assert op == "ls"
+
+    sizes = []
+    for line in outputs:
+        if not line.startswith("dir"):
+            sizes.append(int(line.split(" ")[0]))
+        else:
+            dir_name = line.split(" ")[1]
+            children[abspath].append(f"{abspath}/{dir_name}")
+
+    dir_sizes[abspath] = sum(sizes)
+
+
+for block in blocks:
+    parse(block)
+
+
+# Do DFS
+@lru_cache(None)  # Cache may not be strictly necessary
+def dfs(abspath):
+    size = dir_sizes[abspath]
+    for child in children[abspath]:
+        size += dfs(child)
+    return size
+
+
+ans = 0
+for abspath in dir_sizes:
+    if dfs(abspath) <= 100000:
+        ans += dfs(abspath)
+
+print(ans)
+
+
+unused = 70000000 - dfs("/")
+required = 30000000 - unused
+
+ans = 1 << 60
+for abspath in dir_sizes:
+    size = dfs(abspath)
+    if size >= required:
+        ans = min(ans, size)
+
+print(ans)
